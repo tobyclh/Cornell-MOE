0b8f469b8f164ba430d9d949fd6c408c67672a06
Toby the potato <tobyclh@gmail.com>
2018-02-21 18:02:17 +0900

    Add script to source necessary path for building
    as suggested by the readme

---
+++ ubuntu_path_scipt.sh
@@ -0,0 +1,5 @@
+
+export MOE_CC_PATH=/usr/bin/gcc
+export MOE_CXX_PATH=/usr/bin/g++
+
+export MOE_CMAKE_OPTS="-D MOE_PYTHON_INCLUDE_DIR=/home/toby/anaconda3/include/python3.6m -D MOE_PYTHON_LIBRARY=/home/toby/anaconda3/lib/libpython3.6m.so.1"
